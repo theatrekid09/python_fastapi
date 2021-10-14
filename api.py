c7704ffda356dab59c5716019e4e0dcca53294ab
theatrekid09 <khalifbcooper@outlook.com>
2021-10-14 03:57:13 -0400

    added path parameters for api

--- api.py
+++ api.py
@@ -1,9 +1,23 @@
 # uvicorn api:app --reload 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "james",
+        "age": 25,
+        "major": "biology"
+    }
+}
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="Enter the students id that you want to view")):
+    return students[student_id]
+
